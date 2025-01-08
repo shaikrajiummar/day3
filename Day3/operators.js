86b97f2d4ba4df9c702b6e80976f22d4bbc8df3e
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-01-08 13:21:51 +0530

    message

--- Day3/operators.js
+++ Day3/operators.js
@@ -1,1 +1,3 @@
 console.log(2 ** 5)
+console.log(2 ** 0)
+console.log(2)
---
+++ Day4/looping.js
@@ -0,0 +1,54 @@
+// for (let i = 1; i >= 10; i++) {
+//     console.log("i")
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("i")
+//     if(i%2 ==0)
+//         console.log(i ="i is a even number")
+// }else{
+
+//     console.log(i + "it is a odd number")
+// }
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     //     console.log("it is a odd number");
+//     //     console.log("it is aeven number");
+//     //     console.log("it isa off number")
+//     console.log('i')
+// }
+
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = num; i > 1; i--) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// let a = Number(prompt("Enter a Number:"))
+// let factorial = 1;
+// for (let i = 1; i <= a; i++) {
+//     factorial = i * factorial;
+// }
+// console.log(factorial);
+
+//to find the factors of the number
+// let i = 1;
+// while (i >= 5) {
+//     console.log("helllo ");
+//     i++
+
+// }
+// // do while loop
+// {
+//     console.log("hello javascript")
+// } while (i <= 5);
+function addTocart() {
+    alert("product ia dded to cart successfully")
+    document.body.style.background = "red";
+}
