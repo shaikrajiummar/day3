30020de31c6fa55abf67fb637dbc6dfc94bfaac0
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-01-21 10:42:04 +0530

    message

--- day5/array.js
+++ day5/array.js
@@ -1,16 +1,92 @@
-const users = ["akhil", "sanyu paul", "pranav", "muzahid", "rajiummar"];
-for (let i = 0; i < users.length; i++) {
-    console.log(users[i]);
+//array is the non primitive datatuype
+// let users = ["akhil", "sanyu paul", "pranav", "muzahid", "rajiummar"];
+// for (let i = 0; i < users.length; i++) {//map method:to written the value
+//     console.log(users[i]);
+// }
+// let uppercasedUsers = users.map(user => user.toUpperCase());
+// console.log(uppercasedUsers);
+// users.push("charan sai");
+// console.log(users); 
+// users.pop();
+// console.log(users); 
+// users.unshift("charan sai");
+// console.log(users);
+// users.shift();
+// console.log(users); 
+// let x = users.join("");
+// console.log(x, typeof x); 
+// <h1>20/01/2025</h1>
+// let users = ["akhil", "sanyu paul", "pranav", "muzahid", "rajiummar"];
+// // users.map((user,i)=>{
+// //     console.log(i,user,i)
+
+// })
+// let x = users.map((user,i)=>{
+//     return user;
+
+// });
+// console.log(x)
+// let x=users.forEach((user,i)=>{
+//     // console.log(user);
+//     return user;
+//     // Map():-it is the used to the iterative the values;
+//     // it is returns new copied array from the original array
+//     //forEach():-it it=s used ti iterative the values
+//     // it returns "undefined"
+
+// });
+// console.log(x);
+
+//create array if objects:
+ ///waht is ibject?
+ //object is an entity which contains states and behaviours.
+ //object is an entity to store the valures in the from key and value pair.
+ //crating a object ,class is mandatrity
+ //syntax:- let obj ={}; 
+//  ex:- let userdetails={
+//     name:"ssam",
+//     age:55
+//  }
+//note:- Javascript is mainly object based language till 2015(es6)
+//after es6 feature they added oops concept.
+//nested object
+// let userdetails ={
+//     name:"rajiummar",
+//     age:20,
+//     address:{
+//     city:"Hyderabad"
+    
+//     pincode:{
+//        pin1:789456,
+//        pin2:363255,
+
+
+//       }  }
+// };
+// console.log(userDetails);
+// console.log(userDetails.name);
+// console.log(userDetails.address.city);
+// console.log(userDetails.address.pincode.pin1);
+//!create array of obejets
+let user1={
+    name:"rajiummar",
+    city:"hyd",
+
 }
-let uppercasedUsers = users.map(user => user.toUpperCase());
-console.log(uppercasedUsers);
-users.push("charan sai");
-console.log(users); 
-users.pop();
-console.log(users); 
-users.unshift("charan sai");
-console.log(users);
-users.shift();
-console.log(users); 
-let x = users.join("");
-console.log(x, typeof x); 
+let user2={
+    name:"rajiummar",
+    city:"hyd",
+
+}
+
+let user3={
+    name:"rajiummar",
+    city:"hyd",
+
+}
+let userDetails=[
+ {   name:"rajiummar",
+    city:"hyd"
+},
+userDetails
+]
