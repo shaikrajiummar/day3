0112f66de42080c28ad42b800e52b1573115800a
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-01-24 11:43:00 +0530

    message

---
+++ dom.js
@@ -0,0 +1,46 @@
+// let test = document.getElementById("test");
+// test.innerText ="Header"
+// test.innerText="header"
+// test.innerHTML=<h1>Header</h1>
+// console.log(test)
+
+// let ele =document.getElementsByClassName("test");
+// ele[0].style.backgroundColor = "dodgerblue"
+// //console.log(ele);
+// //console.log(Array.isArray(ele));//this methos is check whether this arrat is pure or
+// //!spread operator:- this operator is used to take out each every value from \
+// //!originsl array  and sotore it  in one more arrary (pure array)
+// //syntax of spread - [...variable]
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// x.map(element => {
+//     element.Style.backgroundColor = "tomato";
+//     element.Style.color = "white";
+//     element.Style.textTransform = "uppercase";
+
+// })
+
+
+
+// let ele = document.getElementsByName("div")
+// //console.log(ele);
+// [...ele].map(element={
+//     console.log(element.innerText);
+
+// })
+
+// let ele = document.querySelector.querySelector("#demo");
+// console.log(ele);
+// let ele = document.querySelector(".test");
+// console.log(ele)
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele)
+
+let demo= document.getElementById('demo');
+demo.addEventListener("click",()=>{
+    console.log("button clickrd")
+    alert('hi');
+    document.body.style.getElementById("demo");
+    
+})
