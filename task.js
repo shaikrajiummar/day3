0e5259225bcd84a27f91e6d1326cdbe96db3db94
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-01-29 12:42:10 +0530

    message

---
+++ task.js
@@ -0,0 +1,10 @@
+let emp1 = {employeeid: "a@gmail.com",employeename: "Raj",company: "Google",  employeesalary: "2000000",empaddress: "Hyd",emp1city: "Hyd"};
+let emp2 = {employeeid: "b@gmail.com",employeename: "Raj",company: "Google",  employeesalary: "2000000",empaddress: "Hyd",emp1city: "Hyd"};
+let emp3 = {employeeid: "c@gmail.com",employeename: "Raj",company: "Google",  employeesalary: "2000000",empaddress: "Hyd",emp1city: "Hyd"};
+let emp4 = {employeeid: "d@gmail.com",employeename: "Raj",company: "Google",  employeesalary: "2000000",empaddress: "Hyd",emp1city: "Hyd"};
+let emp5 = {employeeid: "a@gmail.com",employeename: "Raj",company: "Google",  employeesalary: "2000000",empaddress: "Hyd",emp1city: "Hyd"};
+
+function displayEmployees(employees) {
+    const table = document.createElement("table");
+    table.border = "1";
+}
