c0a037a1c249486ca2ce0ae70470644d19e89273
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-01-31 11:40:56 +0530

    message

---
+++ app.js
@@ -0,0 +1,12 @@
+// import x from "./child1.js"
+// console.log(x)
+
+import college, { x, user, users, obj } from "./child1.js";
+console.log(college);
+console.log(x);
+console.log(user);
+console.log(users);
+users.map(user => {
+    console.log(user);
+})
+console.log(obj.company);
---
+++ child1.js
@@ -0,0 +1,35 @@
+// let x=10;
+// export default x;
+let userDetail={
+    name:"rajiummar",
+    profession:"student"
+}
+//export default userDetail;
+
+// named export
+// let x=10;
+// const user = "ummar";
+// const users=["a","c","e","r","q"];
+// const obj = {
+//     name:"san",
+//     comapny:"Excelr"
+// }
+
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+// }
+
+
+export let x=10;
+export const user = "ummar";
+export const users=["a","c","e","r","q"];
+export const obj = {
+    name:"san",
+    comapny:"Excelr"
+}
+let collage ="MallaReddy University";
+export default collage;
+
