530836f7dc590b1de2148cf3e7408d0c8bde211f
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2025-02-01 22:00:48 +0530

    message

---
+++ training/src/App.js
@@ -0,0 +1,47 @@
+// import React from 'react';
+// function App() {
+//   let username="rajiummar";
+//   return (
+//     <div>
+//      <h1>ExcleR</h1>
+//      <p>para</p>
+//      {username}
+//      {100*100}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React from "react";
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//       <h1>Class component</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+// function App(){
+//   return (
+//     <div>
+//     <h1>function component</h1>
+
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// const App = ()={
+//   return(
+//     <h1> Arreow function</h1>
+//   )
+// }
+
+// export default App;
